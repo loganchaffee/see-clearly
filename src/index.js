063f42e12f047c7245c8f72affa5a5ded4d86fbe
Logan Chaffee <loganschaffee@gmail.com>
2021-09-27 21:13:37 -0700

    zoom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import { Helmet } from 'react-helmet';
 import './styles/index.css';
 import App from './components/App';
 
 import {store} from './store/store'
-
 // store.subscribe(() => console.log(store.getState()))
 
 // store.dispatch(incremented(1))
@@ -15,6 +15,9 @@ import {store} from './store/store'
 
 ReactDOM.render(
     <Provider store={store}>
+        <Helmet>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+        </Helmet>
         <App />
     </Provider>, 
     document.getElementById('root')
